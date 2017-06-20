1eaa8ffa833f1a54b10d2faf58dfea094ac4975b
Miguel <miguelangelsonarc@hotmail.com>
2017-06-20 00:34:27 -0500

    Whipped Cream

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,10 +9,17 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.NumberFormat;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+import static android.util.Log.v;
 
 /**
  * This app displays an order form to order coffee.
@@ -21,20 +28,10 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total $" + price;
-        priceMessage = priceMessage + "\nThank you!";
-        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -56,18 +53,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        CheckBox checkBox = (CheckBox) findViewById(R.id.has_whipped_cream);
+        boolean hasWhippedCreamLog = checkBox.isChecked();
+        int price = calculatePrice();
+        String priceMessage = createOrderSummary(price, hasWhippedCreamLog);
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param quantity    is the number of cups of coffee ordered
+     * @param pricePerCup is the price of one cup of coffee
+     */
+    private int calculatePrice(int quantity, int pricePerCup) {
+        return quantity * pricePerCup;
+    }
+
+    private void calculatePrice(int quantity) {
+        int price = quantity * 5;
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @return total price
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /**
+     * Create summary of the order.
+     *
+     * @param price of the order
+     * @return text summary
+     */
+    private String createOrderSummary(int price, boolean hasWhippedCream) {
+        String priceMessage = "Name: Kaptain Kunal";
+        priceMessage += "\nAdd whipped cream? " + hasWhippedCream;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
     }
 }
